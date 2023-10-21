e451c6198e1012a83599c7f8cdd25c8a227a9e02
Gaurav Vaidya <gaurav@renci.org>
2023-10-21 14:40:53 -0600

    Reformatted test files with black.

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,52 +17,68 @@ import urllib.parse
 
 import requests
 
-CAM_KP_API_ENDPOINT = os.getenv('CAM_KP_API_ENDPOINT', 'https://automat.renci.org/cam-kp/')
-TRAPI_VERSION = os.getenv('TRAPI_VERSION', '1,4')
+CAM_KP_API_ENDPOINT = os.getenv(
+    "CAM_KP_API_ENDPOINT", "https://automat.renci.org/cam-kp/"
+)
+TRAPI_VERSION = os.getenv("TRAPI_VERSION", "1,4")
 
 
 def test_metadata():
     """
     Test the GET /cam-kp/metadata endpoint.
     """
-    metadata_url = urllib.parse.urljoin(CAM_KP_API_ENDPOINT, 'metadata')
+    metadata_url = urllib.parse.urljoin(CAM_KP_API_ENDPOINT, "metadata")
     response = requests.get(metadata_url)
     assert response.ok, f"Could not retrieve {metadata_url}"
 
     metadata = response.json()
 
     # Check some metadata items.
-    assert metadata['graph_id'] == "CAMKP_Automat"
-    assert metadata['graph_name'] == "CAM Provider KG"
-    assert metadata['final_node_count'] > 111_000
-    assert metadata['final_edge_count'] > 3_000_000
+    assert metadata["graph_id"] == "CAMKP_Automat"
+    assert metadata["graph_name"] == "CAM Provider KG"
+    assert metadata["final_node_count"] > 111_000
+    assert metadata["final_edge_count"] > 3_000_000
 
     # Check the sources information.
-    sources = metadata['sources']
+    sources = metadata["sources"]
     assert len(sources) == 1, f"Unexpected number of sources from CAM-KP: {sources}"
-    assert sources[0]['source_id'] == "CAM-KP"
-    assert sources[0]['provenance'] == "infores:go-cam"
-    assert sources[0]['attribution'] == "https://github.com/ExposuresProvider/cam-kp-api"
-    assert sources[0]['source_data_url'] == "https://github.com/ExposuresProvider/cam-kp-api"
-    assert sources[0]['license'] == "https://github.com/ExposuresProvider/cam-kp-api/blob/master/LICENSE"
+    assert sources[0]["source_id"] == "CAM-KP"
+    assert sources[0]["provenance"] == "infores:go-cam"
+    assert (
+        sources[0]["attribution"] == "https://github.com/ExposuresProvider/cam-kp-api"
+    )
+    assert (
+        sources[0]["source_data_url"]
+        == "https://github.com/ExposuresProvider/cam-kp-api"
+    )
+    assert (
+        sources[0]["license"]
+        == "https://github.com/ExposuresProvider/cam-kp-api/blob/master/LICENSE"
+    )
 
     # Check the QC results.
-    assert set(metadata['qc_results']['primary_knowledge_sources']) == {"infores:ctd", "infores:aop-cam", "infores:go-cam"}
+    assert set(metadata["qc_results"]["primary_knowledge_sources"]) == {
+        "infores:ctd",
+        "infores:aop-cam",
+        "infores:go-cam",
+    }
 
-    node_curie_prefixes = metadata['qc_results']['node_curie_prefixes']
+    node_curie_prefixes = metadata["qc_results"]["node_curie_prefixes"]
     assert "CHEBI" in node_curie_prefixes
     assert "NCBIGene" in node_curie_prefixes
     assert "PUBCHEM.COMPOUND" in node_curie_prefixes
     assert "UBERON" in node_curie_prefixes
     assert "UniProtKB" in node_curie_prefixes
 
-    assert set(metadata['qc_results']['edge_properties']) == {
+    assert set(metadata["qc_results"]["edge_properties"]) == {
         "predicate",
         "biolink:primary_knowledge_source",
         "object",
         "subject",
-        "xref"
+        "xref",
     }
 
     # TODO: we need to add infores:aop-cam to https://github.com/biolink/biolink-model/blob/db44be0c49939229c28cbb71a715127941e0ce0b/infores_catalog.yaml
-    assert set(metadata['qc_results']['warnings']['invalid_knowledge_sources']) == {"infores:aop-cam"}
+    assert set(metadata["qc_results"]["warnings"]["invalid_knowledge_sources"]) == {
+        "infores:aop-cam"
+    }
