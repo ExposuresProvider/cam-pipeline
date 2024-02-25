7faf141c0fabf3353ec2efa1aa079781c2484dab
Gaurav Vaidya <gaurav@renci.org>
2024-02-25 00:09:40 -0500

    Added advanced options.

--- frontend/src/components/CAMKPFrontend.vue
+++ frontend/src/components/CAMKPFrontend.vue
@@ -20,6 +20,21 @@
         </p>
       </div>
     </div>
+
+    <div class="accordion" id="advancedOptionsAccordion">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="advancedOptionsHeader">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#advancedOptions" aria-expanded="false" aria-controls="advancedOptions">
+            Advanced options
+          </button>
+        </h2>
+        <div id="advancedOptions" class="accordion-collapse collapse" aria-labelledby="advancedOptionsHeader" data-bs-parent="#advancedOptionsAccordion">
+          <div class="accordion-body">
+            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
