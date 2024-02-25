d1e13cd72b838f6571dbd0ea9adf1b1b347351f2
Gaurav Vaidya <gaurav@renci.org>
2024-02-25 02:43:11 -0500

    Models can be selected and changed.

--- frontend/src/components/ListCAMs.vue
+++ frontend/src/components/ListCAMs.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import {computed, onMounted, ref, watch} from "vue";
+
 export interface Props {
   automatCAMKPEndpoint?: string,
   changeSelectedModel: Function,
@@ -8,9 +10,37 @@ const props = withDefaults(defineProps<Props>(), {
   automatCAMKPEndpoint: 'https://automat.renci.org/cam-kp',
 });
 
-function updateModelList() {
-  props.changeSelectedModel('test');
+async function getModels(limit=100) {
+  // This picks a hundred models so we have something to display.
+  const cypher_endpoint = props.automatCAMKPEndpoint + '/cypher';
+  let response = await fetch(cypher_endpoint, {
+    'method': 'POST',
+    'headers': {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    'body': JSON.stringify({
+      'query': 'MATCH (s)-[p]-(o) RETURN DISTINCT p.xref LIMIT ' + limit,
+    }),
+  });
+  let j: any = await response.json();
+  const rows = j['results'][0]['data'].flatMap(row => row['row'][0][0]);
+  console.log("rows", rows);
+  return rows;
 }
+
+const listedModels = ref([]);
+const selectedModel = ref();
+
+watch(selectedModel, (newlySelectedModel: string) => props.changeSelectedModel(newlySelectedModel));
+
+onMounted(() => {
+  getModels(10).then(modelURLs => {
+    listedModels.value = modelURLs;
+    selectedModel.value = modelURLs[0];
+  });
+})
+
 </script>
 
 <template>
@@ -19,9 +49,16 @@ function updateModelList() {
       <strong>List of CAM Models</strong>
     </div>
     <div class="card-body">
-      <p class="card-text">
-        Based on {{ props.automatCAMKPEndpoint }} and {{ props.changeSelectedModel }}.
-      </p>
+      <ul class="list-group">
+        <li
+            v-for="modelURL in listedModels" :key="modelURL"
+            :class="'list-group-item list- ' + (modelURL == selectedModel ? 'active' : '')"
+            @click="selectedModel = modelURL"
+        >
+          {{modelURL}}
+        </li>
+      </ul>
+
       <a @click="updateModelList()" href="#" class="btn btn-primary">Search</a>
     </div>
   </div>
